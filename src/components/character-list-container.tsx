d16f83c3d511a0555bcedcf1a94984affdba3190
Heath Volmer <hvolmer@pepperdash.com>
2021-10-03 14:20:31 -0600

    Populated character lists and link to next component

--- src/components/character-list-container.tsx
+++ src/components/character-list-container.tsx
@@ -1,43 +1,54 @@
 import { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
-import { useLocation } from "react-router";
-import apiService from "../services/api.service";
+import { useHistory, useLocation } from "react-router";
 import { Person } from "../types/person";
 
 export function CharacterListContainer(props: CharacterListContainerProps) {
   const query = new URLSearchParams(useLocation().search);
-  let species = query.get('species');
+  let speciesUrl = query.get('species') || '';
   const [characters, setCharacters] = useState<Person[]>([]);
   useEffect(() => {
-    console.log("Will retrieve " + species);
-    (async () => {
-      const { data } = await apiService.get(species!);
-    })();
+    console.log("Will retrieve " + speciesUrl);
+    const matchingChars = props.characters.filter(c => c.species.includes(speciesUrl));
+    setCharacters(matchingChars);
+  }, []);
+
+  const history = useHistory();
+  const items = characters.map(c => {
+    // I'm using the url and router to try to retain some history
+    const id = c.url.split('/').slice(-2)[0];
+    return (
+      <div key={c.url} onClick={() => history.push(`/characters/${id}`)}>
+        <span className="text-light">{c.name}</span>
+      </div>
+    )
   });
 
   return (
     <div>
-      <p>People list container... Species: {species}</p>
-      <Row className="bg-primary py-2 mb-2">
-        <Col>
-          <button className="btn btn-sm btn-light">Prev</button>
-          <button className="btn btn-sm btn-light ml-2">Next</button>
-        </Col>
-      </Row>
+      <ListNav />
       <Row>
         <Col>
           <div className="card-grid">
-            {characters}
+            {items}
           </div>
         </Col>
       </Row>
-      <Row className="bg-primary py-2 my-2">
-        <Col>
-          <button className="btn btn-sm btn-light">Prev</button>
-          <button className="btn btn-sm btn-light ml-2">Next</button>
-        </Col>
-      </Row>
+      <ListNav />
     </div>
+  );
+}
+
+function ListNav() {
+  return (
+    <Row className="bg-secondary py-2 my-2">
+      <Col>
+        <button className="btn btn-sm btn-light">Prev</button>
+        {/* Can't get the ml-2 to work! */}
+        &nbsp;
+        <button className="btn btn-sm btn-light ml-2">Next</button>
+      </Col>
+    </Row>
   );
 }
 
